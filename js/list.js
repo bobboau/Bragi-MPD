4d012271977ab6e6c3b7ae3ff30da521f97c6c43
Mike Abegg <bobboau@gmail.com>
2015-11-22 01:06:15 -0500

    changed list updating to be progressive/timer based so it was more responsive, especialy on mobile

--- js/list.js
+++ js/list.js
@@ -2,6 +2,15 @@
  * sets up list controls. List controls are song/directory lists for representing MPD's Queue, Playlist, and search results
  */
 (function(){
+
+    /**
+     * @var int - number of UI elements to display at a time
+     */
+    var UPDATE_BATCH = 32;
+
+    /*
+     * automatic setup
+     */
     $(function(){
         //setup any exsisting lists
         $('.LIST').each(function(i,e){setupList(e)});
@@ -30,18 +39,49 @@
         $(contents).find('.LIST_header').replaceWith(header);
         $(contents).find('.LIST_empty').replaceWith(empty);
 
+        /**
+         *@var timer - timer handle for the next time we add elements to the list. null means we are not adding
+         */
+        contents.update_timer = null;
+
         //now add a bunch of methods to it
         contents.setItems = function(items){
+            //if we are in the middle of updating something else stop that
+            if(contents.update_timer){
+                clearTimeout(contents.update_timer);
+                contents.update_timer = null;
+            }
+            //clear what ever is there already
             $(contents).find('.LIST_contents').empty();
+            //if we are adding a whole lot of nothing have the UI relfect this
             if(items.length < 1){
                 $(contents).find('.LIST_empty').addClass('LIST_is_actually_empty');
             }
             else{
+                //the expected case of us adding a bunch of stuff
+                //don't have the UI say there is nothing
                 $(contents).find('.LIST_empty').removeClass('LIST_is_actually_empty');
-                items.forEach(function(item){
-                    var item_content = item.getItemUI();
-                    $(contents).find('.LIST_contents').append(item_content);
-                });
+                //setup an interval for adding stuff, one huge monolithic add is unresponsive for long lists of things
+                var count = 0;//which timer iteration are we on
+                contents.update_timer = setInterval(function(){
+                    for(var i = 0; i<UPDATE_BATCH; i++){
+                        //for however many items we are doing per-update add stuff
+                        var offset = count*UPDATE_BATCH+i;     //get which item we are on
+                        if(offset < items.length){
+                            //as long as we haven't gone off the end of the list
+                            var item = items[count*UPDATE_BATCH+i];     //get the item
+                            var item_content = item.getItemUI();        //add it
+                            $(contents).find('.LIST_contents').append(item_content);
+                        }
+                        else{
+                            //if we have gone off the end of the list stop doing stuff
+                            clearTimeout(contents.update_timer);
+                            contents.update_timer = null;
+                            return;
+                        }
+                    }
+                    count++;
+                }, 100);
             }
         };
 
